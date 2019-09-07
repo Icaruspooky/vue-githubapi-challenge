cdc9d30f47ff1532a17c1a7d60fc5e9574028d06
Ícaro Cristofolini <icaro.cristofolini@projuris.com.br>
2019-09-07 00:17:28 -0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <h1>GitHub</h1>
+      <h2>Veja os repositórios de algum usuário:</h2>
+    </div>
+    <div>
+      <Index></Index>
+    </div>
+  </div>
+</template>
+
+<script>
+import Index from "@/views/Index.vue";
+
+export default {
+  components: {
+    Index
+  }
+};
+</script>
---
+++ src/views/Index.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <div>
+      <input
+        type="text"
+        placeholder="Digite o nome do usuário"
+        v-model="username"
+        @keyup.enter="getUser(username)"
+        data-test="entrada"
+      >
+    </div>
+    <div>
+      <table>
+        <thead>
+          <th>Nome</th>
+          <th>Link</th>
+          <th>Stars</th>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(repositorie, index) in repositories"
+            :key="index"
+            data-test="repositorio"
+          >
+            <td>{{ repositorie.name }}</td>
+            <td>{{ repositorie.html_url }}</td>
+            <td>{{ repositorie.stargazers_count }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <span
+        v-if="error"
+        data-test="nao-encontrado"
+      >{{error}}</span>
+      <span
+        v-if="empty"
+        data-test="sem-repositorios"
+      >{{empty}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { maxLength } from "vuelidate/lib/validators";
+
+export default {
+  components: {},
+  data() {
+    return {
+      username: "",
+      repositories: [],
+      error: "",
+      empty: ""
+    };
+  },
+  validations: {
+    username: {
+      correctCharacters(username) {
+        return (
+          username.match(/^[0-9a-z\-]+$/) &&
+          (username.match(/-/g) || []).length <= 1 &&
+          username.charAt(0) != "-" &&
+          username.charAt(username.length - 1) != "-"
+        );
+      },
+      maxLength: maxLength(39)
+    }
+  },
+  methods: {
+    async getUser(username) {
+      if (username.trim() && !this.$v.username.$invalid) {
+        try {
+          let { data } = await axios.get(
+            `https://api.github.com/users/${username}/repos`
+          );
+          if (data.length) {
+            console.log("successfull");
+            this.empty = "";
+            this.error = "";
+            this.repositories = data;
+          } else {
+            console.log("empty");
+            this.error = "";
+            this.repositories = [];
+            this.empty = "Usuário não possui repositórios.";
+          }
+        } catch (err) {
+          this.empty = "";
+          this.repositories = [];
+          this.error = err.response.data.message;
+        }
+      }
+    }
+  }
+};
+</script>
